8be82816c2ac17cf2dc9d9ee4a727d47cedfddc2
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-09-10 01:47:36 +0500

    optimized sidebar on mobile screen

--- src/components/Global/Sidebar.jsx
+++ src/components/Global/Sidebar.jsx
@@ -4,38 +4,17 @@ import { Link, useNavigate } from "react-router-dom";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Sidebar = ({ handleShowNav }) => {
-  //   const [showNav, setShowNav] = useState(false);
-  //   const handleShowNav = () => {
-  //     setShowNav(!showNav);
-  //   };
   const [showNav, setShowNav] = useState(false);
-  const [scroll, setScroll] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const navigate = useNavigate();
-  const [showContactPage, setShowContactPage] = useState(false);
   const [showPayNowLinks, setShowPayNowLinks] = useState(false);
   const [openWeightLossDropdown, setOpenWeightLossDropdown] = useState(false);
   const [openPeptideDropdown, setOpenPeptideDropdown] = useState(false);
-
-  const handleNavigateToAboutPage = () => {
-    navigate("/about");
-  };
+  const [usageDirectionDropdown, setUsageDirectionDropdown] = useState(false);
 
   const handleMouseEnter = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  const handleMouseLeave = () => {
-    setIsDropdownOpen(false);
-  };
-
-  const handleShowPayNowLinksMouseEnter = () => {
-    setShowPayNowLinks(true);
-  };
-
-  const handleShowPayNowLinksMouseLeave = () => {
-    setShowPayNowLinks(false);
-  };
   return (
     <div className="w-[90%] md:w-[50%] h-full bg-white float-end relative pt-16 shadow-2xl">
       <button
@@ -75,57 +54,37 @@ const Sidebar = ({ handleShowNav }) => {
         </div>
         <div className="w-full">
           {isDropdownOpen && (
-            <div
-              className="rounded-md bg-white flex flex-col items-start"
-              onClick={handleMouseEnter}
-            >
+            <div className="rounded-md bg-white flex flex-col items-start">
               <Link
                 to="/testosterone-replacement-therapy/"
-                className={`w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2`}
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
               >
                 Testosterone Therapy
               </Link>
               <Link
                 to="/hcg-therapy/"
-                className={`w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2`}
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
               >
                 HCG Therapy
               </Link>
               <div className="w-full relative z-20">
                 <button
                   onClick={() => setOpenPeptideDropdown(!openPeptideDropdown)}
-                  onMouseLeave={() => setOpenPeptideDropdown(false)}
-                  className={`w-full text-[15px] text-start block font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2 z-20`}
+                  className="w-full text-[15px] text-start font-semibold flex items-center justify-between uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2 z-20"
                 >
-                  Peptide Therapy
+                  Peptide Therapy <MdKeyboardArrowDown />
                 </button>
                 {openPeptideDropdown && (
-                  <div
-                    className=" bg-white border w-full pr-4 z-20 flex flex-col items-start"
-                    onMouseEnter={() => setOpenPeptideDropdown(true)}
-                    onMouseLeave={() => setOpenPeptideDropdown(false)}
-                  >
+                  <div className="bg-white w-full pr-4 z-20 flex flex-col items-start">
                     <Link
                       to="/tesamorelin-therapy/"
-                      className={`block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-10 py-2`}
+                      className="block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-10 py-2"
                     >
                       Tesamorelin Therapy
                     </Link>
-                    {/* <Link
-                        to="/peptide-therapy/"
-                        className={`w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2`}
-                      >
-                        BPC-157 Therapy
-                      </Link>
-                      <Link
-                        to="/peptide-therapy/"
-                        className={`w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2`}
-                      >
-                        CJC-1295 Therapy
-                      </Link> */}
                     <Link
                       to="/sermorelin-therapy/"
-                      className={`text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-10 py-2`}
+                      className="text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-10 py-2"
                     >
                       Sermorelin Therapy
                     </Link>
@@ -134,27 +93,24 @@ const Sidebar = ({ handleShowNav }) => {
               </div>
               <div className="w-full relative z-10">
                 <button
-                  onMouseEnter={() => setOpenWeightLossDropdown(true)}
-                  onMouseLeave={() => setOpenWeightLossDropdown(false)}
-                  className={`w-full block text-[15px] text-start font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2`}
+                  onClick={() =>
+                    setOpenWeightLossDropdown(!openWeightLossDropdown)
+                  }
+                  className="w-full text-[15px] text-start font-semibold uppercase hover:text-[#c00000] flex items-center justify-between transition-all duration-300 px-6 py-2"
                 >
-                  Weight loss Therapy
+                  Weight loss Therapy <MdKeyboardArrowDown />
                 </button>
                 {openWeightLossDropdown && (
-                  <div
-                    className="rounded-md bg-white border py-6 w-60 shadow-xl absolute top-6 flex flex-col items-start gap-4 -right-40 z-10"
-                    onMouseEnter={() => setOpenWeightLossDropdown(true)}
-                    onMouseLeave={() => setOpenWeightLossDropdown(false)}
-                  >
+                  <div className="bg-white w-full pr-4 z-20 flex flex-col items-start px-4">
                     <Link
                       to="/semaglutide-therapy/"
-                      className={`w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6`}
+                      className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6"
                     >
                       Semaglutide Therapy
                     </Link>
                     <Link
                       to="/tirzepatide-therapy/"
-                      className={`w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6`}
+                      className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6"
                     >
                       Tirzepatide Therapy
                     </Link>
@@ -164,18 +120,90 @@ const Sidebar = ({ handleShowNav }) => {
             </div>
           )}
         </div>
-        <Link
-          to="/"
-          className="font-bold text-base uppercase border-b py-3 w-full block"
-        >
-          labs
-        </Link>
-        <Link
-          to="/"
-          className="font-bold text-base uppercase border-b py-3 w-full block"
-        >
-          pay now
-        </Link>
+
+        <div className="w-full">
+          <button
+            onClick={() => setShowPayNowLinks(!showPayNowLinks)}
+            className="font-bold text-base uppercase border-b py-3 w-full text-start flex items-center justify-between"
+          >
+            Pay Now <MdKeyboardArrowDown className="text-xl" />
+          </button>
+          {showPayNowLinks && (
+            <div className="rounded-md bg-white flex flex-col items-start">
+              <Link
+                to="/new-patients"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                New Patient
+              </Link>
+              <Link
+                to="/trt-pep-refill-validation/"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                Existing Patient
+              </Link>
+            </div>
+          )}
+        </div>
+
+        <div className="w-full relative">
+          <button
+            onClick={() => setUsageDirectionDropdown(!usageDirectionDropdown)}
+            className="font-bold text-base uppercase border-b py-3 w-full text-start flex items-center justify-between"
+          >
+            Usage Direction <MdKeyboardArrowDown className="text-xl" />
+          </button>
+          {usageDirectionDropdown && (
+            <div
+              className="rounded-md bg-white px-2 pt-3 w-72 z-40 top-6 flex flex-col items-start"
+              onMouseEnter={() => setUsageDirectionDropdown(true)}
+              onMouseLeave={() => setUsageDirectionDropdown(false)}
+            >
+              <a
+                href="TESTOSTERONE.pdf"
+                download="TESTOSTERONE.pdf"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                Testosterone Application
+              </a>
+              <a
+                href="HCG.pdf"
+                download="HCG.pdf"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                HCG Application
+              </a>
+              <a
+                href="CJC-SERMORELIN.pdf"
+                download="CJC-SERMORELIN.pdf"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                Sermorelin Application
+              </a>
+              <a
+                href="CJC-SERMORELIN.pdf"
+                download="CJC-SERMORELIN.pdf"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                Tesamorelin Application
+              </a>
+              <a
+                href="SEMAGLUTIDE-TIRZEPATIDE.pdf"
+                download="SEMAGLUTIDE-TIRZEPATIDE.pdf"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                Semaglutide Application
+              </a>
+              <a
+                href="SEMAGLUTIDE-TIRZEPATIDE.pdf"
+                download="SEMAGLUTIDE-TIRZEPATIDE.pdf"
+                className="w-full block text-[15px] font-semibold uppercase hover:text-[#c00000] transition-all duration-300 px-6 py-2"
+              >
+                Tirzepatide Application
+              </a>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
