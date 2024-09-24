c02604654dfd380f5f674770f43622f9e8595076
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-09-25 01:43:02 +0500

    changed map

--- src/pages/aboutus/Map.jsx
+++ src/pages/aboutus/Map.jsx
@@ -15,7 +15,7 @@ const Map = () => {
   return (
     <>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2918.0272478749325!2d-78.7144629!3d42.99876079999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d374263d7c5b2f%3A0x3ea312c25f2f2818!2s105%20Galileo%20Dr%2C%20Buffalo%2C%20NY%2014221%2C%20USA!5e0!3m2!1sen!2s!4v1722362916086!5m2!1sen!2s"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2919.0228121422497!2d-78.6860212!3d42.97779310000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d375b927ce35bf%3A0x3d3f5974077bac2a!2s8265%20Sheridan%20Dr%2C%20Buffalo%2C%20NY%2014221%2C%20USA!5e0!3m2!1sen!2s!4v1727210393888!5m2!1sen!2s"
         height="240"
         style={{ border: 0, width: "100%" }}
         allowfullscreen=""
